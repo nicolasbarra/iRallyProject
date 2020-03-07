8c8f7028dbdd42a4d8c3b4ad918de235845d197f
Nicolas Barra <nb.nicolasbarra@gmail.com>
2020-03-07 04:57:08 -0500

    use test API to verify route communcation between react and server

--- irallyreactapp/src/App.js
+++ irallyreactapp/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+// TODO: consider removing src/App.css, src/App.test.js, and src/index.css
+
 function App() {
+  // for testing api response
+  const [apiResponse, setAPIResponse] = useState("");
+
+  // for testing api response
+  useEffect(() => {
+    fetch("http://localhost:9000/testAPI").then(res => res.text()).then(res => setAPIResponse(res));
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/*This line below is for TESTING Api response (line below), remove later*/}
+        <p className="App-intro">;{apiResponse}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
