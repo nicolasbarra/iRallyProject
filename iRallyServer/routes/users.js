e14f19bba7bf5f7a6161d495857565bd383da76e
Nicolas Barra <nb.nicolasbarra@gmail.com>
2020-03-18 02:26:12 -0400

    begin working on routes such that they use controllers

--- iRallyServer/routes/users.js
+++ iRallyServer/routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const user_controller = require('../controllers/userController');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
+  // TODO: some kind of error because I don't think this page should happen
   res.send('respond with a resource');
 });
+
+// router.post('/create', user_controller.create_user);
 
 module.exports = router;
