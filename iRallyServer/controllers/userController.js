6235399f34b4b679e87bae6dca4a0476ca78aba1
Nicolas Barra <nb.nicolasbarra@gmail.com>
2020-03-18 02:44:31 -0400

    begin controller for users with validations

--- iRallyServer/controllers/userController.js
+++ iRallyServer/controllers/userController.js
@@ -1,2 +1,22 @@
+const { body, check, validationResult } = require("express-validator");
+
 const User = require('../models/user');
 
+// exports.create_user = [
+// //     check('username').isString().notEmpty().trim().isLength({ min: 3 }).escape(),
+// //     check('password').isString().notEmpty().trim().isLength({ min: 3 }).escape(),
+// //     check('name').isString().notEmpty().trim().isLength({ min: 3 }.escape()),
+// //     check('email').isString().notEmpty().trim().isLength({ min: 3 }).isEmail().normalizeEmail(),
+// //     check('gender').notEmpty().escape(),
+// //     check('genderPronouns').notEmpty().escape(),
+// //     check('interests').isString().notEmpty().trim().isLength({ min: 3 }).escape(),
+// //     (req, res) => {
+// //         const errors = validationResult(req);
+// //         if (!errors.isEmpty()) {
+// //             // TODO: this may be incorrect
+// //             return res.status(422).json({ errors: errors.array() });
+// //         } else {
+// //             // TODO: route content here
+// //         }
+// //     }
+// // ];
