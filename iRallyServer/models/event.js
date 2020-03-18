a5880734bef09da11fd93ffc5d23fef76f773f49
Nicolas Barra <nb.nicolasbarra@gmail.com>
2020-03-17 23:03:16 -0400

    added validation messages and refractor

--- iRallyServer/models/event.js
+++ iRallyServer/models/event.js
@@ -6,33 +6,33 @@ const eventSchema = new Schema(
     {
         eventId: {
             type: String,
-            required: true,
+            required: [true, 'event needs an id (should be unique)'],
             unique: true,
             trim: true
         },
         eventName: {
             type: String,
-            required: true,
+            required: [true, 'event needs a name'],
             trim: true,
             minlength: 3
         },
         creator: {
             type: mongoose.Schema.Types.Mixed,
             // used mixed type as the creator can be an admin or a user
-            required: true,
+            required: [true, 'events must have a creator']
         },
         description: {
             type: String,
-            required: true,
+            required: [true, 'events need descriptions'],
             minlength: 3
         },
         address: {
             type: String,
-            required: true
+            required: [true, 'events need addresses']
         },
         dateTime: {
             type: Date,
-            required: true
+            required: [true, 'events need a date and time']
         },
         attendees: [
             {
@@ -42,7 +42,7 @@ const eventSchema = new Schema(
         ],
         numberOfAttendees: {
             type: Number,
-            required: true,
+            required: [true, 'events must have number of attendees, may be zero']
         },
         comments: [
             {
@@ -56,6 +56,4 @@ const eventSchema = new Schema(
     }
 );
 
-const Event = mongoose.model('Event', eventSchema);
-
-module.exports = Event;
+module.exports = mongoose.model('Event', eventSchema);
