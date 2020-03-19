5c4b81f5e1eb52a95ebcf15d3c5feaa1d4b3bae3
Nicolas Barra <nb.nicolasbarra@gmail.com>
2020-03-19 02:42:01 -0400

    being Kotlin Android app

---
+++ iRally/app/src/main/java/edu/upenn/cis350/irally/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package edu.upenn.cis350.irally.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+        val userId: String,
+        val displayName: String
+)
