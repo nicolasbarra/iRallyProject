0f401e4fe3ee738c43996c11698a0684e9fb591e
Hayley Sussman <hayleysuss@gmail.com>
2020-04-03 17:01:50 -0400

    added event page skeleton

--- iRally/app/src/main/java/edu/upenn/cis350/irally/ui/profile/ProfileActivity.kt
+++ iRally/app/src/main/java/edu/upenn/cis350/irally/ui/profile/ProfileActivity.kt
@@ -1,17 +1,20 @@
 package edu.upenn.cis350.irally.ui.profile
+
 import android.content.Intent
 import android.os.Bundle
 import android.util.Patterns
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.toolbox.NetworkImageView
+import com.squareup.picasso.Picasso
 import edu.upenn.cis350.irally.R
 import edu.upenn.cis350.irally.ui.event.CreateEventActivity
 import edu.upenn.cis350.irally.ui.login.LoginActivity
 import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.activity_profile_picture.view.*
 import kotlinx.android.synthetic.main.activity_register.*
+import org.json.JSONArray
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -25,6 +28,9 @@ class ProfileActivity : AppCompatActivity() {
         val editProfilePicture = edit
         val profilePronouns = profile_pronouns
         val profileName = profile_name
+        val editPicture = edit
+        val linearLayout = event_button_layout
+
 
         val createEvent = create_event_from_profile
         create_event_from_profile.setOnClickListener {
@@ -34,7 +40,24 @@ class ProfileActivity : AppCompatActivity() {
             startActivity(intent);
         }
 
+        edit.setOnClickListener {
+            val intent = Intent(LoginActivity.context, ProfilePicture::class.java)
+            startActivity(intent);
 
+        }
+
+        fun createEventButtons(events: JSONArray) {
+            for (i in 0 until events.length()){
+                val button = Button(this)
+                button.layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+                //val name = events.getJSONObject(i).eventName
+
+
+            }
+        }
 
 
     }
---
+++ iRally/app/src/main/java/edu/upenn/cis350/irally/ui/profile/ProfilePicture.kt
@@ -0,0 +1,97 @@
+package edu.upenn.cis350.irally.ui.profile
+
+import edu.upenn.cis350.irally.R
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_profile.*
+import java.io.IOException
+
+class ProfilePicture : AppCompatActivity() {
+    private lateinit var imageView: ImageView
+    private lateinit var imageButton: Button
+    private lateinit var sendButton: Button
+    private var imageData: ByteArray? = null
+
+    //what goes here???
+    private val postURL: String = "http://10.0.2.2:9000/users" // remember to use your own api
+
+    companion object {
+        private const val IMAGE_PICK_CODE = 999
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_picture)
+
+        imageView = findViewById(R.id.imageView)
+
+        imageButton = findViewById(R.id.imageButton)
+        imageButton.setOnClickListener {
+            launchGallery()
+        }
+        sendButton = findViewById(R.id.sendButton)
+        sendButton.setOnClickListener {
+            uploadImage()
+        }
+    }
+
+    private fun launchGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    private fun uploadImage() {
+        imageData?: return
+        val request = object : VolleyFileUploadRequest(
+            Request.Method.POST,
+            postURL,
+            Response.Listener {
+                println("response is: $it")
+            },
+            Response.ErrorListener {
+                println("error is: $it")
+            }
+        ) {
+            override fun getByteData(): MutableMap<String, FileDataPart> {
+                var params = HashMap<String, FileDataPart>()
+                params["imageFile"] = FileDataPart("image", imageData!!, "jpeg")
+                return params
+            }
+        }
+        //I CHANGED THIS
+        MySingleton.getInstance(this).addToRequestQueue(request)
+    }
+
+    @Throws(IOException::class)
+    private fun createImageData(uri: Uri) {
+        val inputStream = contentResolver.openInputStream(uri)
+        inputStream?.buffered()?.use {
+            imageData = it.readBytes()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            val uri = data?.data
+            if (uri != null) {
+                imageView.setImageURI(uri)
+                createImageData(uri)
+                //HOPE THIS WORKKS
+
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+
+}
