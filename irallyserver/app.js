0069941df9c3bb28c638b69412d1749cde7a2c69
Nicolas Barra <nb.nicolasbarra@gmail.com>
2020-04-05 21:44:52 -0400

    add route for events

--- irallyserver/app.js
+++ irallyserver/app.js
@@ -12,6 +12,7 @@ const cors = require("cors");
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const adminsRouter = require('./routes/admins');
+const eventsRouter = require('./routes/events');
 const fileRouter = require('./routes/imageUpload');
 
 const app = express();
@@ -36,7 +37,8 @@ app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admins', adminsRouter);
-app.use('/files', fileRouter)
+app.use('/admins', eventsRouter);
+app.use('/files', fileRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
