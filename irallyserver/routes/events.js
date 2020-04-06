dfa79dee86afdc93e6ab0193033dbb3d8f581332
Nicolas Barra <nb.nicolasbarra@gmail.com>
2020-04-05 21:43:25 -0400

    add create event route

--- irallyserver/routes/events.js
+++ irallyserver/routes/events.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const event_controller = require('../controllers/userController');
+
 /* GET events listing. */
 router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 });
+
+router.post('/create', event_controller.create_event);
 
 module.exports = router;
