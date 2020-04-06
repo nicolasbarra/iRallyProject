d24b21e3ce3932a51971bf2d8557a00e57c38741
Nicolas Barra <nb.nicolasbarra@gmail.com>
2020-04-06 18:53:58 -0400

    added attendee route

--- irallyserver/routes/events.js
+++ irallyserver/routes/events.js
@@ -7,4 +7,6 @@ router.post('/', event_controller.event);
 
 router.post('/create', event_controller.create_event);
 
+router.post('/addAttendee', event_controller.add_attendee);
+
 module.exports = router;
