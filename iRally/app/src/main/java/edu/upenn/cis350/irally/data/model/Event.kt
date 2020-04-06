3b3a936469fd98670db6eb85bed5f7b9e54affd0
Hayley Sussman <hayleysuss@gmail.com>
2020-04-05 20:31:58 -0400

    create event object

--- iRally/app/src/main/java/edu/upenn/cis350/irally/data/model/Event.kt
+++ iRally/app/src/main/java/edu/upenn/cis350/irally/data/model/Event.kt
@@ -1,4 +1,13 @@
 package edu.upenn.cis350.irally.data.model
 
-class Event {
-}
+data class Event(
+    val eventId: String,
+    val eventName: String,
+    val creator: LoggedInUser,
+    val description: String,
+    val address: String,
+    val dateTime: String,
+    val attendees: String,
+    val numberOfAttendees: Int,
+    val interestsOfAttendees: String
+)
