736d591cd42e195804a1265711cd78664e1b5a5e
Nicolas Barra <nb.nicolasbarra@gmail.com>
2020-04-06 11:28:42 -0400

    refractor

--- iRally/app/src/main/java/edu/upenn/cis350/irally/data/LoginRepository.kt
+++ iRally/app/src/main/java/edu/upenn/cis350/irally/data/LoginRepository.kt
@@ -14,15 +14,6 @@ class LoginRepository() {
         var user: LoggedInUser? = null
             private set
 
-        val isLoggedIn: Boolean
-            get() = user != null
-
-        init {
-            // If user credentials will be cached in local storage, it is recommended it be encrypted
-            // @see https://developer.android.com/training/articles/keystore
-            user = null
-        }
-
         fun logout() {
             user = null
         }
