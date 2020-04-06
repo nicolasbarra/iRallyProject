64b9cf49247e224e1b7faaf84872119d9e196b60
Nicolas Barra <nb.nicolasbarra@gmail.com>
2020-04-06 18:57:33 -0400

    prepare delete event

--- irallyserver/routes/events.js
+++ irallyserver/routes/events.js
@@ -7,6 +7,8 @@ router.post('/', event_controller.event);
 
 router.post('/create', event_controller.create_event);
 
+// router.post('/delete', event_controller.create_event);
+
 router.post('/addAttendee', event_controller.add_attendee);
 
 module.exports = router;
