c47b2e96cba7fd4323a5573738dae5a58ac8a9f0
Nicolas Barra <nb.nicolasbarra@gmail.com>
2020-04-06 17:51:23 -0400

    set event page default values based on stored values from server

--- iRally/app/src/main/java/edu/upenn/cis350/irally/ui/event/EventPageActivity.kt
+++ iRally/app/src/main/java/edu/upenn/cis350/irally/ui/event/EventPageActivity.kt
@@ -2,19 +2,34 @@ package edu.upenn.cis350.irally.ui.event
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ExpandableListView
 import androidx.appcompat.app.AppCompatActivity
 import edu.upenn.cis350.irally.R
+import edu.upenn.cis350.irally.data.EventRepository
 import kotlinx.android.synthetic.main.activity_event_page.*
 import org.json.JSONObject
 
-class EventPageActivity: AppCompatActivity() {
+class EventPageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_event_page)
 
-        val eventName = event_page_name
+        event_page_name.text =
+            EventRepository.eventSelected?.eventId ?: "Error: Unable to set event."
+
+        event_page_creator.text =
+            EventRepository.eventSelected?.creator ?: "Error: creator cannot be found"
+
+        event_page_description.text =
+            EventRepository.eventSelected?.description ?: "Error: description cannot be found"
+
+        event_page_address.text =
+            EventRepository.eventSelected?.address ?: "Error: address cannot be found"
+
+        event_page_date.text =
+            EventRepository.eventSelected?.dateTime ?: "Error date and time cannot be found"
 
         val markAsGoing = event_page_button
 
