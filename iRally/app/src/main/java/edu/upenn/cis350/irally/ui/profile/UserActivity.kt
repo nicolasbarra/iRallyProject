1304d38e3e638b2bfdb7481893bcf90fb7b1bd58
Amelia Rosenbaum <mimi2440@gmail.com>
2020-04-20 02:48:46 -0400

    Fill the user profile page with the values returned from the route

--- iRally/app/src/main/java/edu/upenn/cis350/irally/ui/profile/UserActivity.kt
+++ iRally/app/src/main/java/edu/upenn/cis350/irally/ui/profile/UserActivity.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
 import edu.upenn.cis350.irally.R
+import edu.upenn.cis350.irally.data.repository.LoginRepository
 import edu.upenn.cis350.irally.ui.feed.FeedActivity
 import edu.upenn.cis350.irally.ui.search.SearchActivity
 import kotlinx.android.synthetic.main.activity_user.*
@@ -16,6 +18,16 @@ class UserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
+
+        val userInfo = LoginRepository.userSelected!!
+        user_profile_name.text = userInfo.displayName
+        user_profile_pronouns.text = userInfo.genderPronouns
+        user_profile_interests.text =
+            userInfo.interests.toString().filter { e -> e != '[' && e != ']' }
+        if (!userInfo.profilePictureLink.isNullOrEmpty()) {
+            Picasso.with(this).load(userInfo.profilePictureLink)
+                .into(profile_picture)
+        }
 
         friend.setOnClickListener {
             // TODO: set friend button onclick listener
