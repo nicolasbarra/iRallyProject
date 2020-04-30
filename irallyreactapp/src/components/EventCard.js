59ab9cfe1b690f0894d745166e7a3b24bd196553
Nicolas Barra <nb.nicolasbarra@gmail.com>
2020-04-30 12:45:49 -0500

    remove unused import

--- irallyreactapp/src/components/EventCard.js
+++ irallyreactapp/src/components/EventCard.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom';
 import "./EventCard.css"
-import axios from 'axios';
 
 class EventCard extends Component {
     // deleteEvent = () => {
