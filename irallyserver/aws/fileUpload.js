1edd0fb297c944128fc68ab330cbb2697fc0a849
nicolasbarra <47286193+nicolasbarra@users.noreply.github.com>
2020-06-03 00:18:07 -0400

    remove codes

--- irallyserver/aws/fileUpload.js
+++ irallyserver/aws/fileUpload.js
@@ -2,9 +2,9 @@ const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
  aws.config.update({
-    secretAccessKey: "BB2IEerAEOe+gP5/jL3ztAr0Pq73v0n67iL9LvS6",
-    accessKeyId: "AKIAJMHJG5IZYFCOQA3A",
-    region: 'us-east-1'
+    secretAccessKey: "",
+    accessKeyId: "",
+    region: ''
  });
 const s3 = new aws.S3();
  
